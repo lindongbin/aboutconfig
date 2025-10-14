15d20759bc0efdda1d16a85d7107e93963821854
LDB <lin.crk@gmail.com>
2025-10-14 20:09:57 +0800

    Update HistoryInNewTab.uc.js

--- HistoryInNewTab.uc.js
+++ HistoryInNewTab.uc.js
@@ -4,7 +4,7 @@
     const originalWhereToOpenLink = BrowserUtils.whereToOpenLink;
 
     BrowserUtils.whereToOpenLink = function(event, ignoreButton, ignoreAlt) {
-        if (event?.target?.closest('#historyMenuPopup, #PanelUI-history')) {
+        if (event?.target?.closest('#historyMenuPopup, #PanelUI-history, #historyTree')) {
             if (gBrowser.selectedTab.isEmpty) {
                 return "current";
             }
