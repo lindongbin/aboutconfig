32a6e9ab2dbdbff61cfce57f93ab7ead99ae7006
LDB <lin.crk@gmail.com>
2025-12-10 19:56:18 +0800

    Create config-prefs.js

---
+++ install_folder/defaults/pref/config-prefs.js
@@ -0,0 +1,5 @@
+// Only for UserChromeRules.uc.js
+
+pref("general.config.obscure_value", 0);
+pref("general.config.filename", "UserChromeRules.uc.js");
+pref("general.config.sandbox_enabled", false);
