6644fd55f06a720a968301ce0f7875f1b946a7f3
LDB <lin.crk@gmail.com>
2026-06-04 10:11:49 +0800

    Update HistoryInNewTab.uc.js

--- HistoryInNewTab.uc.js
+++ HistoryInNewTab.uc.js
@@ -4,7 +4,7 @@
     const originalWhereToOpenLink = BrowserUtils.whereToOpenLink;
 
     BrowserUtils.whereToOpenLink = function(event, ignoreButton, ignoreAlt) {
-        if (event?.target?.closest('#historyMenuPopup, #PanelUI-history, #historyTree')) {
+        if (event?.target?.closest('#historyMenuPopup, #PanelUI-history, #historyTree') || event?.target?.ownerDocument?.URL === 'chrome://browser/content/sidebar/sidebar-history.html') {
             if (gBrowser.selectedTab.isEmpty) {
                 return "current";
             }
